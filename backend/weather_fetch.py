59f9816835d531c6fb8668136a10f4fe7fadcce1
Aaron <asintayehu@proton.me>
2025-02-18 12:51:29 -0500

    chore: moved python files to backend folder for organization

--- backend/weather_fetch.py
+++ backend/weather_fetch.py
@@ -9,34 +9,48 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Purpose of this class: handle user input
+class PromptHandler:
+    def __init__(self):
+        self = self
+    def get_zip_code():
+        return input("Enter zip code here: ")
+
+class APIClient:
+    def __init__(self, base_url):
+        self.base_url = base_url
+    
+    def call_api_url(self):
+        return requests.get(self.base_url)
+
 nomi = pgeocode.Nominatim('us')
 
-zip_code = input("Enter zip code here: ")
+x = PromptHandler.get_zip_code()
 
-# Fetching latitude and longitude based on zip code
-query = nomi.query_postal_code(zip_code)
+# Parsing geocode API Data
+query = nomi.query_postal_code(x)
 data = { "lat": query["latitude"], "lon": query["longitude"] }
 latitude = str(data["lat"]).strip()
 longitude = str(data["lon"]).strip()
-
-# Fetching city name
 location = query['place_name']
+API_KEY = os.getenv('API_KEY')
 
-API_KEY = os.getenv("API_KEY")
+client = APIClient(f'https://api.openweathermap.org/data/2.5/weather?lat={latitude}8&lon={longitude}&appid={API_KEY}&units=imperial')
 
-x = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={latitude}8&lon={longitude}&appid={API_KEY}&units=imperial')
-
+# Calling and JSON-ify the Weather API data
+x = client.call_api_url()
 weather_data = x.json()
 
-app = Flask(__name__)
 
+# Parsing the Weather API data
 sunrise = strftime('%-I:%M%p', localtime(weather_data['sys']['sunrise']))
 sunset = strftime('%-I:%M%p', localtime(weather_data['sys']['sunset']))
 date = strftime('%Y-%m-%d', localtime(weather_data['sys']['sunset']))
-
 weather_data['sys']['sunrise'] = sunrise
 weather_data['sys']['sunset'] = sunset
 
+# Start of flask process
+app = Flask(__name__)
 @app.route('/')
 def hello():
     return render_template('template.html', weather=weather_data['main'], location=weather_data['name'], wind_speed=weather_data['wind']['speed'],
